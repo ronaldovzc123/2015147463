a06b9e94e4f19b54dee3f6270a19dbcfbce9f7aa
Your Name <ronaldov_zelada@hotmail.com>
2017-04-13 02:36:42 -0500

    falta solo consola

--- 2015147463/2015147463/administrativo.cs
+++ 2015147463/2015147463/administrativo.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace _2015147463
 {
-    class administrativo
+    class administrativo : Empleado
     {
+        private int IDadmin { set; get; }
+
+        public administrativo(int idadmin, int idemp) : base(idemp) 
+        {
+            idadmin = IDadmin;
+            idemp = IDemp;
+        }
+
     }
 }
