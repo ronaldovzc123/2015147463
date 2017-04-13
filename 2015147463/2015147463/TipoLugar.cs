6cc7070f8cbaf0a631ea1aa1fe9f07be168d60fb
Your Name <ronaldov_zelada@hotmail.com>
2017-04-12 21:54:22 -0500

    solo creacion de clases

---
+++ 2015147463/2015147463/TipoLugar.cs
@@ -0,0 +1,12 @@
+﻿Lusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015147463
+{
+    class TipoLugar
+    {
+    }
+}
