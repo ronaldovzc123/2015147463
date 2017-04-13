ec325923ce0de00e17e75a0064ec762eba0294ec
Your Name <ronaldov_zelada@hotmail.com>
2017-04-12 22:36:10 -0500

    relaciones de agregacion

--- 2015147463/2015147463/Transporte.cs
+++ 2015147463/2015147463/Transporte.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace _2015147463
 {
-    class Transporte
+    public class Transporte : Servicio
     {
+        private List<LugarViaje> _LugarViaje;
+        private List<cliente> _Cliente;
+        private List<TipoViaje> _tipoViaje;
+        //  agregacion bus
+        private List<Bus> _bus;
+        public List<Bus> Bus
+        {
+            get { return _bus; }
+
+            set
+            {
+                if (value.Count > 0)
+                    _bus = value;
+            }
+ 
+        }
+
     }
 }
